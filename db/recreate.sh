16e3eb282e40ef66bad0de02d67f551153ec61f0
Aleksandar Rajic <aleksandar.v.rajic@gmail.com>
2021-04-13 22:08:08 +0200

    restructuring

--- db/recreate.sh
+++
@@ -1,5 +0,0 @@
-echo "> Recreating database: masterdb"
-echo "> Please enter password for the user: root"
-psql -U root -W -p 5003 -h 127.0.0.1 -e  -d masterdb -f ./db/Master-2021_04_08_21_59_51-dump.sql
-echo "> Database successfully recreated: masterdb"
-
